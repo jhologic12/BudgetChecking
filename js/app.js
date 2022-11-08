cfd8a8c731571bb1280a77310324a1eb41ce15c9
jhologic12 <jhonospino@gmail.com>
2022-11-07 19:38:25 -0500

    creacion de arreglos ingreso y egresos

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+const ingresos = [
+ new Ingreso ('Salario',3000.00),
+ new Ingreso ('Venta Software',1200)
+];
+
+
+const egresos = [
+    new Egreso ('Renta Departamento', 900),
+    new Egreso ('Viajes',1000)
+]
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+
+let cargarCabecero = ()=>{
+
+    
+}
