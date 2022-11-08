1b110da26e0b4412bae6d3bf3e490005b0b8a2c2
jhologic12 <jhonospino@gmail.com>
2022-11-07 20:31:54 -0500

    agregando formato moneda y porcentaje

--- js/app.js
+++ js/app.js
@@ -10,12 +10,58 @@ const egresos = [
     new Egreso ('Viajes',1000)
 ]
 
-let cargarApp = ()=>{
+let LoadAPP =()=>{
+    console.log("cargando app");
     cargarCabecero();
 }
 
+let totalIngresos = () =>{
+
+    let totalIngresos = 0;
+
+    for(let ingreso of ingresos){
+
+        totalIngresos += ingreso.valor;
+    }
+
+    return totalIngresos;
+}
+
+let totalEgresos = () => {
+
+    let totalEgresos = 0;
+
+    for (let egreso of egresos){
+        totalEgresos += egreso.valor;
+    }
+
+    return totalEgresos;
+}
+
+
 
 let cargarCabecero = ()=>{
+    console.log("cargando cabecero");
 
-    
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgresos = totalEgresos() / totalIngresos ();
+
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgresos);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos()) ;
+
+
+}
+
+
+const formatoMoneda = (valor) =>{
+
+  return  valor.toLocaleString('en-US',{style:'currency', currency:'USD',minimumFractionDigits:2});
+
+}
+
+const formatoPorcentaje = (valor)=>{
+
+      return  valor.toLocaleString('en-US',{style:'percent' , minimumFractionDigits:2});
 }
